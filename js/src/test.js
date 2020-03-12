91123bb702953d05963592712bf35762b47090e1
jeje <goasb18@gmail.com>
2020-03-12 09:21:49 +0900

    퇴사

---
+++ js/src/test.js
@@ -0,0 +1,109 @@
+$(document).ready(function(){
+	$(document).on("mouseover", "#belgium", function(){
+		$("#belgium > a > p").addClass("color");
+		$("#belgium > a > img").attr('src','/resource/project/200302_flag/belgium_hover.png');
+	});
+		$(document).on("mouseover", "#bulgraria", function(){
+		$("#bulgraria > a > p").addClass("color");
+		$("#bulgraria > a > img").attr('src','/resource/project/200302_flag/bulgraria_hover.png');
+	});
+		$(document).on("mouseover", "#croatia", function(){
+		$("#croatia > a > p").addClass("color");
+		$("#croatia > a > img").attr('src','/resource/project/200302_flag/croatia_hover.png');
+	});
+		$(document).on("mouseover", "#serbia", function(){
+		$("#serbia > a > p").addClass("color");
+		$("#serbia > a > img").attr('src','/resource/project/200302_flag/serbia_hover.png');
+	});
+		$(document).on("mouseover", "#slovenia", function(){
+		$("#slovenia > a > p").addClass("color");
+		$("#slovenia > a > img").attr('src','/resource/project/200302_flag/slovenia_hover.png');
+	});
+		$(document).on("mouseover", "#czech republic", function(){
+		$("#czech republic > a > p").addClass("color");
+		$("#czech republic > a > img").attr('src','/resource/project/200302_flag/czech-republic_hover.png');
+	});
+		$(document).on("mouseover", "#denmark", function(){
+		$("#denmark > a > p").addClass("color");
+		$("#denmark > a > img").attr('src','/resource/project/200302_flag/denmark_hover.png');
+	});
+		$(document).on("mouseover", "#egypt", function(){
+		$("#egypt > a > p").addClass("color");
+		$("#egypt > a > img").attr('src','/resource/project/200302_flag/egypt_hover.png');
+	});
+		$(document).on("mouseover", "#estonia", function(){
+		$("#estonia > a > p").addClass("color");
+		$("#estonia > a > img").attr('src','/resource/project/200302_flag/estonia_hover.png');
+	});
+		$(document).on("mouseover", "#finaland", function(){
+		$("#finaland > a > p").addClass("color");
+		$("#finaland > a > img").attr('src','/resource/project/200302_flag/finaland_hover.png');
+	});
+		$(document).on("mouseover", "#greece", function(){
+		$("#greece > a > p").addClass("color");
+		$("#greece > a > img").attr('src','/resource/project/200302_flag/greece_hover.png');
+	});
+		$(document).on("mouseover", "#hungary", function(){
+		$("#hungary > a > p").addClass("color");
+		$("#hungary > a > img").attr('src','/resource/project/200302_flag/hungary_hover.png');
+	});
+		$(document).on("mouseover", "#israel", function(){
+		$("#israel > a > p").addClass("color");
+		$("#israel > a > img").attr('src','/resource/project/200302_flag/israel_hover.png');
+	});
+		$(document).on("mouseover", "#Italy", function(){
+		$("#Italy > a > p").addClass("color");
+		$("#Italy > a > img").attr('src','/resource/project/200302_flag/ltaly_hover.png');
+	});
+		$(document).on("mouseover", "#Iithania", function(){
+		$("#Iithania > a > p").addClass("color");
+		$("#Iithania > a > img").attr('src','/resource/project/200302_flag/lithania_hover.png');
+	});
+		$(document).on("mouseover", "#netherlands", function(){
+		$("#netherlands > a > p").addClass("color");
+		$("#netherlands > a > img").attr('src','/resource/project/200302_flag/netherlands_hover.png');
+	});
+		$(document).on("mouseover", "#norway", function(){
+		$("#norway > a > p").addClass("color");
+		$("#norway > a > img").attr('src','/resource/project/200302_flag/norway_hover.png');
+	});
+		$(document).on("mouseover", "#poland", function(){
+		$("#poland > a > p").addClass("color");
+		$("#poland > a > img").attr('src','/resource/project/200302_flag/poland_hover.png');
+	});
+		$(document).on("mouseover", "#portugal", function(){
+		$("#portugal > a > p").addClass("color");
+		$("#portugal > a > img").attr('src','/resource/project/200302_flag/portugal_hover.png');
+	});
+		$(document).on("mouseover", "#switzerland", function(){
+		$("#switzerland > a > p").addClass("color");
+		$("#switzerland > a > img").attr('src','/resource/project/200302_flag/switzerland_hover.png');
+	});
+		$(document).on("mouseover", "#turkey", function(){
+		$("#turkey > a > p").addClass("color");
+		$("#turkey > a > img").attr('src','/resource/project/200302_flag/turkey_hover.png');
+	});
+		$(document).on("mouseover", "#ukraine", function(){
+		$("#ukraine > a > p").addClass("color");
+		$("#ukraine > a > img").attr('src','/resource/project/200302_flag/ukraine_hover.png');
+	});
+		$(document).on("mouseover", "#united kingdom", function(){
+		$("#united kingdom > a > p").addClass("color");
+		$("#united kingdom > a > img").attr('src','/resource/project/200302_flag/united-kingdom_hover.png');
+	});
+		$(document).on("mouseover", "#sweden", function(){
+		$("#sweden > a > p").addClass("color");
+		$("#sweden > a > img").attr('src','/resource/project/200302_flag/sweden_hover.png');
+    });
+    
+
+
+
+    // -----------------------------------------------------------------------------------------
+
+
+    $(document).on("mouseout", "#belgium", function(){
+		$("#belgium > a > p").removeClass("color");
+		$("#belgium > a > img").attr('src','/resource/project/200302_flag/Belgium.png');
+	});
+});
---
+++ js/src/test_02.js
@@ -0,0 +1,244 @@
+$(document).ready(function(){
+	$(document).on("mouseover", "#belgium", function(){
+		$("#belgium > a > p").addClass("color");
+		$("#belgium > a > img").attr('src','/resource/project/200302_flag/belgium_hover.png');
+	});
+		$(document).on("mouseover", "#bulgraria", function(){
+		$("#bulgraria > a > p").addClass("color");
+		$("#bulgraria > a > img").attr('src','/resource/project/200302_flag/bulgraria_hover.png');
+	});
+		$(document).on("mouseover", "#croatia", function(){
+		$("#croatia > a > p").addClass("color");
+		$("#croatia > a > img").attr('src','/resource/project/200302_flag/croatia_hover.png');
+	});
+		$(document).on("mouseover", "#serbia", function(){
+		$("#serbia > a > p").addClass("color");
+		$("#serbia > a > img").attr('src','/resource/project/200302_flag/serbia_hover.png');
+	});
+		$(document).on("mouseover", "#slovenia", function(){
+		$("#slovenia > a > p").addClass("color");
+		$("#slovenia > a > img").attr('src','/resource/project/200302_flag/slovenia_hover.png');
+	});
+		$(document).on("mouseover", "#czech republic", function(){
+		$("#czech republic > a > p").addClass("color");
+		$("#czech republic > a > img").attr('src','/resource/project/200302_flag/czech-republic_hover.png');
+	});
+		$(document).on("mouseover", "#denmark", function(){
+		$("#denmark > a > p").addClass("color");
+		$("#denmark > a > img").attr('src','/resource/project/200302_flag/denmark_hover.png');
+	});
+		$(document).on("mouseover", "#egypt", function(){
+		$("#egypt > a > p").addClass("color");
+		$("#egypt > a > img").attr('src','/resource/project/200302_flag/egypt_hover.png');
+	});
+		$(document).on("mouseover", "#estonia", function(){
+		$("#estonia > a > p").addClass("color");
+		$("#estonia > a > img").attr('src','/resource/project/200302_flag/estonia_hover.png');
+	});
+		$(document).on("mouseover", "#finaland", function(){
+		$("#finaland > a > p").addClass("color");
+		$("#finaland > a > img").attr('src','/resource/project/200302_flag/finaland_hover.png');
+	});
+		$(document).on("mouseover", "#greece", function(){
+		$("#greece > a > p").addClass("color");
+		$("#greece > a > img").attr('src','/resource/project/200302_flag/greece_hover.png');
+	});
+		$(document).on("mouseover", "#hungary", function(){
+		$("#hungary > a > p").addClass("color");
+		$("#hungary > a > img").attr('src','/resource/project/200302_flag/hungary_hover.png');
+	});
+		$(document).on("mouseover", "#israel", function(){
+		$("#israel > a > p").addClass("color");
+		$("#israel > a > img").attr('src','/resource/project/200302_flag/israel_hover.png');
+	});
+		$(document).on("mouseover", "#Italy", function(){
+		$("#Italy > a > p").addClass("color");
+		$("#Italy > a > img").attr('src','/resource/project/200302_flag/ltaly_hover.png');
+	});
+		$(document).on("mouseover", "#Iithania", function(){
+		$("#Iithania > a > p").addClass("color");
+		$("#Iithania > a > img").attr('src','/resource/project/200302_flag/lithania_hover.png');
+	});
+		$(document).on("mouseover", "#netherlands", function(){
+		$("#netherlands > a > p").addClass("color");
+		$("#netherlands > a > img").attr('src','/resource/project/200302_flag/netherlands_hover.png');
+	});
+		$(document).on("mouseover", "#norway", function(){
+		$("#norway > a > p").addClass("color");
+		$("#norway > a > img").attr('src','/resource/project/200302_flag/norway_hover.png');
+	});
+		$(document).on("mouseover", "#poland", function(){
+		$("#poland > a > p").addClass("color");
+		$("#poland > a > img").attr('src','/resource/project/200302_flag/poland_hover.png');
+	});
+		$(document).on("mouseover", "#portugal", function(){
+		$("#portugal > a > p").addClass("color");
+		$("#portugal > a > img").attr('src','/resource/project/200302_flag/portugal_hover.png');
+	});
+		$(document).on("mouseover", "#switzerland", function(){
+		$("#switzerland > a > p").addClass("color");
+		$("#switzerland > a > img").attr('src','/resource/project/200302_flag/switzerland_hover.png');
+	});
+		$(document).on("mouseover", "#turkey", function(){
+		$("#turkey > a > p").addClass("color");
+		$("#turkey > a > img").attr('src','/resource/project/200302_flag/turkey_hover.png');
+	});
+		$(document).on("mouseover", "#ukraine", function(){
+		$("#ukraine > a > p").addClass("color");
+		$("#ukraine > a > img").attr('src','/resource/project/200302_flag/ukraine_hover.png');
+	});
+		$(document).on("mouseover", "#united kingdom", function(){
+		$("#united kingdom > a > p").addClass("color");
+		$("#united kingdom > a > img").attr('src','/resource/project/200302_flag/united-kingdom_hover.png');
+	});
+		$(document).on("mouseover", "#indonesia", function(){
+		$("#indonesia > a > p").addClass("color");
+		$("#indonesia > a > img").attr('src','/resource/project/200302_flag/indonesia_hover.png');
+    });
+		$(document).on("mouseover", "#malaysia", function(){
+		$("#malaysia > a > p").addClass("color");
+		$("#malaysia > a > img").attr('src','/resource/project/200302_flag/malaysia_hover.png');
+    });
+		$(document).on("mouseover", "#new_zealand", function(){
+		$("#new_zealand > a > p").addClass("color");
+		$("#new_zealand > a > img").attr('src','/resource/project/200302_flag/new_zealand_hover.png');
+    });
+		$(document).on("mouseover", "#pakistan", function(){
+		$("#pakistan > a > p").addClass("color");
+		$("#pakistan > a > img").attr('src','/resource/project/200302_flag/pakistan_hover.png');
+    });
+		$(document).on("mouseover", "#philippines", function(){
+		$("#philippines > a > p").addClass("color");
+		$("#philippines > a > img").attr('src','/resource/project/200302_flag/philippines_hover.png');
+    });
+		$(document).on("mouseover", "#thailand", function(){
+		$("#thailand > a > p").addClass("color");
+		$("#thailand > a > img").attr('src','/resource/project/200302_flag/thailand_hover.png');
+    });
+
+
+    // -----------------------------------------------------------------------------------------
+
+$(document).ready(function(){
+	$(document).on("mouseout", "#belgium", function(){
+		$("#belgium > a > p").removeClass("color");
+		$("#belgium > a > img").attr('src','/resource/project/200302_flag/belgium.png');
+	});
+		$(document).on("mouseout", "#bulgraria", function(){
+		$("#bulgraria > a > p").removeClass("color");
+		$("#bulgraria > a > img").attr('src','/resource/project/200302_flag/bulgraria.png');
+	});
+		$(document).on("mouseout", "#croatia", function(){
+		$("#croatia > a > p").removeClass("color");
+		$("#croatia > a > img").attr('src','/resource/project/200302_flag/croatia.png');
+	});
+		$(document).on("mouseout", "#serbia", function(){
+		$("#serbia > a > p").removeClass("color");
+		$("#serbia > a > img").attr('src','/resource/project/200302_flag/serbia.png');
+	});
+		$(document).on("mouseout", "#slovenia", function(){
+		$("#slovenia > a > p").removeClass("color");
+		$("#slovenia > a > img").attr('src','/resource/project/200302_flag/slovenia.png');
+	});
+		$(document).on("mouseout", "#czech republic", function(){
+		$("#czech republic > a > p").removeClass("color");
+		$("#czech republic > a > img").attr('src','/resource/project/200302_flag/czech-republic.png');
+	});
+		$(document).on("mouseout", "#denmark", function(){
+		$("#denmark > a > p").removeClass("color");
+		$("#denmark > a > img").attr('src','/resource/project/200302_flag/denmark.png');
+	});
+		$(document).on("mouseout", "#egypt", function(){
+		$("#egypt > a > p").removeClass("color");
+		$("#egypt > a > img").attr('src','/resource/project/200302_flag/egypt.png');
+	});
+		$(document).on("mouseout", "#estonia", function(){
+		$("#estonia > a > p").removeClass("color");
+		$("#estonia > a > img").attr('src','/resource/project/200302_flag/estonia.png');
+	});
+		$(document).on("mouseout", "#finaland", function(){
+		$("#finaland > a > p").removeClass("color");
+		$("#finaland > a > img").attr('src','/resource/project/200302_flag/finaland.png');
+	});
+		$(document).on("mouseout", "#greece", function(){
+		$("#greece > a > p").removeClass("color");
+		$("#greece > a > img").attr('src','/resource/project/200302_flag/greece.png');
+	});
+		$(document).on("mouseout", "#hungary", function(){
+		$("#hungary > a > p").removeClass("color");
+		$("#hungary > a > img").attr('src','/resource/project/200302_flag/hungary.png');
+	});
+		$(document).on("mouseout", "#israel", function(){
+		$("#israel > a > p").removeClass("color");
+		$("#israel > a > img").attr('src','/resource/project/200302_flag/israel.png');
+	});
+		$(document).on("mouseout", "#Italy", function(){
+		$("#Italy > a > p").removeClass("color");
+		$("#Italy > a > img").attr('src','/resource/project/200302_flag/ltaly.png');
+	});
+		$(document).on("mouseout", "#Iithania", function(){
+		$("#Iithania > a > p").removeClass("color");
+		$("#Iithania > a > img").attr('src','/resource/project/200302_flag/lithania.png');
+	});
+		$(document).on("mouseout", "#netherlands", function(){
+		$("#netherlands > a > p").removeClass("color");
+		$("#netherlands > a > img").attr('src','/resource/project/200302_flag/netherlands.png');
+	});
+		$(document).on("mouseout", "#norway", function(){
+		$("#norway > a > p").removeClass("color");
+		$("#norway > a > img").attr('src','/resource/project/200302_flag/norway.png');
+	});
+		$(document).on("mouseout", "#poland", function(){
+		$("#poland > a > p").removeClass("color");
+		$("#poland > a > img").attr('src','/resource/project/200302_flag/poland.png');
+	});
+		$(document).on("mouseout", "#portugal", function(){
+		$("#portugal > a > p").removeClass("color");
+		$("#portugal > a > img").attr('src','/resource/project/200302_flag/portugal.png');
+	});
+		$(document).on("mouseout", "#switzerland", function(){
+		$("#switzerland > a > p").removeClass("color");
+		$("#switzerland > a > img").attr('src','/resource/project/200302_flag/switzerland.png');
+	});
+		$(document).on("mouseout", "#turkey", function(){
+		$("#turkey > a > p").removeClass("color");
+		$("#turkey > a > img").attr('src','/resource/project/200302_flag/turkey.png');
+	});
+		$(document).on("mouseout", "#ukraine", function(){
+		$("#ukraine > a > p").removeClass("color");
+		$("#ukraine > a > img").attr('src','/resource/project/200302_flag/ukraine.png');
+	});
+		$(document).on("mouseout", "#united kingdom", function(){
+		$("#united kingdom > a > p").removeClass("color");
+		$("#united kingdom > a > img").attr('src','/resource/project/200302_flag/united-kingdom.png');
+	});
+		$(document).on("mouseout", "#sweden", function(){
+		$("#sweden > a > p").removeClass("color");
+        $("#sweden > a > img").attr('src','/resource/project/200302_flag/sweden.png');
+    });
+        $(document).on("mouseout", "#indonesia", function(){
+        $("#indonesia > a > p").removeClass("color");
+        $("#indonesia > a > img").attr('src','/resource/project/200302_flag/indonesia.png');
+    });
+        $(document).on("mouseout", "#malaysia", function(){
+        $("#malaysia > a > p").removeClass("color");
+        $("#malaysia > a > img").attr('src','/resource/project/200302_flag/malaysia.png');
+    });
+        $(document).on("mouseout", "#new_zealand", function(){
+        $("#new_zealand > a > p").removeClass("color");
+        $("#new_zealand > a > img").attr('src','/resource/project/200302_flag/new_zealand.png');
+    });
+        $(document).on("mouseout", "#pakistan", function(){
+        $("#pakistan > a > p").removeClass("color");
+        $("#pakistan > a > img").attr('src','/resource/project/200302_flag/pakistan.png');
+    });
+        $(document).on("mouseout", "#philippines", function(){
+        $("#philippines > a > p").removeClass("color");
+        $("#philippines > a > img").attr('src','/resource/project/200302_flag/philippines.png');
+    });
+        $(document).on("mouseout", "#thailand", function(){
+        $("#thailand > a > p").removeClass("color");
+        $("#thailand > a > img").attr('src','/resource/project/200302_flag/thailand.png');
+        });
+    });
+
